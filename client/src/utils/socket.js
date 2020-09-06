1077b316a93e4287e5c4520af513026a2bf93156
JY <cjyclx@gmail.com>
2020-09-06 22:44:10 +0800

    test client deploy

--- client/src/utils/socket.js
+++ client/src/utils/socket.js
@@ -1,7 +1,6 @@
-// var socket = require('socket.io-client')('http://localhost:1337');
+var socket = require('socket.io-client')('http://localhost:1337');
 
-var socket = require('socket.io-client')('https://sm-social-media.herokuapp.com/socket.io/?EIO=4&transport=websocket');
+// var socket = require('socket.io-client')('https://sm-social-media.herokuapp.com/socket.io/?EIO=4&transport=websocket');
 
-// const socket = openSocket('https://sm-social-media.herokuapp.com');
 
 export default socket
