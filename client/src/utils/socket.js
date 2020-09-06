cdf3a77b3820be330034284030725b0aea8fc9be
JY <cjyclx@gmail.com>
2020-09-06 19:30:55 +0800

    test deploy socket

--- client/src/utils/socket.js
+++ client/src/utils/socket.js
@@ -1,6 +1,6 @@
 // var socket = require('socket.io-client')('ws://localhost:1337');
 
-var socket = require('socket.io-client')('ws://sm-social-media.herokuapp.com/socket.io/?EIO=4&transport=websocket');
+var socket = require('socket.io-client')('https://sm-social-media.herokuapp.com/socket.io/?EIO=4&transport=websocket');
 
 // const socket = openSocket('https://sm-social-media.herokuapp.com');
 
