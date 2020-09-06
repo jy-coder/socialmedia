4cdad60d85ec89697c5cc34dc9c3a246410e51db
JY <cjyclx@gmail.com>
2020-09-07 04:25:38 +0800

    test deploy

--- client/src/utils/socket.js
+++ client/src/utils/socket.js
@@ -2,8 +2,9 @@
 // var socket = require('socket.io-client')('https://sm-social-media.herokuapp.com/socket.io/?EIO=4&transport=websocket');
 
 
-const URL = 'http://localhost:1337'
-// const URL = 'https://sm-social-media.herokuapp.com/socket.io/?EIO=4&transport=websocket'
+
+// const URL = 'http://localhost:1337'
+const URL = 'https://sm-social-media.herokuapp.com'
 let io;
 
 module.exports = {
